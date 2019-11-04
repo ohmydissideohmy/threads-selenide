e7df0491aa3cca03a61503f4a6b886688f4afb7b
artsiom.vinahradau <artsiom.vinahradau@external.wirecard.com>
2019-11-04 13:51:03 +0300

    Small ref

--- src/test/java/tests/TestRunner.java
+++ src/test/java/tests/TestRunner.java
@@ -3,7 +3,6 @@ package tests;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-import tests.suites.AfishaSuite;
 import tests.suites.LoginSuite;
 import tests.suites.RatingsSuite;
 
